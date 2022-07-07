eebbe2d1b2448f552947270f896fe33c2492cc43
Julieta De Diago <jdediago@invertironline.com>
2022-07-07 02:17:24 -0300

    segundaentrega

--- carrito.js
+++ carrito.js
@@ -1,8 +1,52 @@
 // Obtenemos los elementos de html.
 let inputProducto = document.getElementById("inputProducto");
-const inputCantidad = document.getElementById("inputCantidad");
+const selectCantidad = document.getElementById("selectCantidad");
 const contenedor = document.querySelector("#contenedor");
-const btnAgregar = document.querySelector("#btnAgregar");
+const contenedorProductos = document.getElementById("contenedorProductos");
+
+function agregarAlCarrito(id) {
+    console.log("Agregando")
+    let productoAgregar = stock.find(element => element.id == id);
+    console.log(productoAgregar);
+}
+
+function mostrarProductos(listaDeProductos){
+
+    listaDeProductos.forEach(element => {
+        let html = `
+        <div class="card" style="width: 18rem;">
+            <img src="${element.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title" id="inputProducto">${element.nombreProducto}</h5>
+            <p class="card-text">${element.descripcion}</p>
+            <p class="card-text">${element.presentación} </p>
+            <p class="card-text">$${element.precio}</p>
+                <label for="unidades" class="form-label">Unidades que queres llevar</label>
+                <select class="form-select" aria-label="select unidades" id="selectCantidad">
+                <option selected>x1</option>
+                <option value="1">Pack de 10</option>
+                <option value="2">Pack de 25</option>
+                <option value="3">Pack de 100</option>
+            </select>
+            <br>
+            <a class="btn btn-primary" id="btnAgregar${element.id}">Agregar</a>  
+            <br>
+            </div>
+        </div>`;
+
+        contenedorProductos.innerHTML+=html;
+    
+        let btnAgregarId = document.getElementById(`btnAgregar${element.id}`);
+        btnAgregarId.addEventListener("click", () => {
+            agregarAlCarrito(element.id)
+        }) 
+    });
+}
+
+
+
+
+mostrarProductos(stock)
 
 //Creamos el html con las cosas que nos envian por parametro.
 function agregarProducto(producto, cantidad){
@@ -11,18 +55,17 @@ function agregarProducto(producto, cantidad){
     // "Hola, esto es un string" --> "hola" + producto + "esto es un string"
     // 'Hola, esto es un string'
 
-    html =  "El producto es " + producto + " seleccionaste " + cantidad + " unidades";
+    html =  "<li>El producto es " + producto + " seleccionaste " + cantidad + " unidades</li>";
 
 
     contenedor.innerHTML += html;
 }
 
-//Funcion que envia a agregarProducto su valor.
-function submit(){
-    agregarProducto(inputProducto.value, inputCantidad.value);
-}
 
-// Accion de click.
-btnAgregar.addEventListener("click", () => {
-    submit()
-})
+// Accion de click.inputProducto.value
+// btnAgregar.addEventListener("click", () => {
+   
+//     agregarProducto(inputProducto.textContent, selectCantidad.options[selectCantidad.selectedIndex].text);
+// })
+
+
