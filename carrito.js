ac09c4c26cfd0c422f348ba72b332281e4a76771
Julieta De Diago <jdediago@invertironline.com>
2022-07-31 02:28:49 -0300

    Funciona carrito oka

--- carrito.js
+++ carrito.js
@@ -26,18 +26,24 @@ function importarStock() {
 //funcion constructora
 function Producto(producto, cantidad) {
     this.id = producto.id
+    this.nombre = producto.nombreProducto
     this.precio = producto.precio
     this.cantidad = cantidad
 }
 
 function agregarProducto(producto, cantidad){
-    let html;
     let productoAAgregar = new Producto(producto, cantidad);
+    
+    carritoProductosAgregados.push(productoAAgregar);
+    actualizarCarrito();
+    mostrarCarrito(productoAAgregar);    
+}
 
-    html = `<li>El producto es <b> ${producto.nombreProducto}</b> <p id="cantidad${productoAAgregar.id}"> seleccionaste ${productoAAgregar.cantidad} unidades.</p> El precio de cada unidad es: $ ${productoAAgregar.precio} <a class='btn btn-danger btn-sm' id='botonEliminar${productoAAgregar.id}'><i class='bi bi-trash-fill'></i></a> </li>`;
+function mostrarCarrito(productoAAgregar) {
+    let html;
+    html = `<li>El producto es <b> ${productoAAgregar.nombre}</b> <p id="cantidad${productoAAgregar.id}"> seleccionaste ${productoAAgregar.cantidad} unidades.</p> El precio de cada unidad es: $ ${productoAAgregar.precio} <a class='btn btn-danger btn-sm' id='botonEliminar${productoAAgregar.id}'><i class='bi bi-trash-fill'></i></a> </li>`;
 
     contenedor.innerHTML += html;
-    carritoProductosAgregados.push(productoAAgregar);
 
     carritoProductosAgregados.forEach((producto) => {
         let btnEliminar= document.getElementById(`botonEliminar${producto.id}`);
@@ -46,8 +52,6 @@ function agregarProducto(producto, cantidad){
             eliminarProducto(btnEliminar, producto);
         });
     });
-
-    actualizarCarrito();
 }
 
 function eliminarProducto(btnEliminar, productoAAgregar) {
@@ -74,24 +78,24 @@ function mostrarProductos(listaDeProductos){
 
     listaDeProductos.forEach(element => {
         let html = `
-        <div class="card" style="width: 18rem;">
-            <img src="${element.imagen}" class="card-img-top" alt="...">
-            <div class="card-body">
-            <h5 class="card-title" id="inputProducto">${element.nombreProducto}</h5>
-            <p class="card-text">${element.descripcion}</p>
-            <p class="card-text">${element.presentación} </p>
-            <p class="card-text">$${element.precio}</p>
-                <label for="unidades" class="form-label">Unidades que queres llevar</label>
-                <select class="form-select" aria-label="select unidades" id="selectCantidad${element.id}">
-                <option selected>1</option>
-                <option value="1">10</option>
-                <option value="2">25</option>
-                <option value="3">100</option>
-            </select>
-            <br>
-            <a class="btn btn-primary" id="btnAgregar${element.id}">Agregar</a>  
-            <br>
-            </div>
+            <div class="card h-90" style="width: 18rem; margin:10px">
+                <img src="${element.imagen}" class="card-img-top" alt="..." height="220px">
+                <div class="card-body">
+                <h5 class="card-title" id="inputProducto">${element.nombreProducto}</h5>
+                <p class="card-text" id="descripcion">${element.descripcion}</p>
+                <p class="card-text" id="presentacion">${element.presentación} </p>
+                <p class="card-text" id="precio">$${element.precio}</p>
+                    <label for="unidades" class="form-label">Unidades que queres llevar</label>
+                    <select class="form-select" aria-label="select unidades" id="selectCantidad${element.id}">
+                    <option selected>1</option>
+                    <option value="1">10</option>
+                    <option value="2">25</option>
+                    <option value="3">100</option>
+                    </select>
+                <br>
+                <a class="btn btn-primary" id="btnAgregar${element.id}">Agregar</a>  
+                <br>
+                </div>
         </div>`;
 
         contenedorProductos.innerHTML+=html;
@@ -102,7 +106,7 @@ function mostrarProductos(listaDeProductos){
         btnAgregarId.addEventListener("click", () => {
             agregarAlCarrito(producto.id);
             Toastify({
-                text: "Agregaste " + producto.nombreProducto + " al carrito. Que lo disfrutes!",
+                text: "Agregaste " + producto.nombreProducto + " al carrito",
                 duration: 3000,
                 destination: "/#carrito"
               }).showToast();
